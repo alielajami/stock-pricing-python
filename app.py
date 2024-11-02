134fbc4f167fcf044ed7df6b4c9e97dc4557c596
alielajami <alielajami@outlook.com>
2024-11-02 17:07:37 +0200

    added each days's price to each day in a list

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 
 days = int(input("Enter the number of days: "))
+prices = []
 
-for i in range(days):
-    input("Enter the price of the stock on day", i, ": ")
+for i in range(1, days+1):
+    price = int(input("Enter the price of the stock on day " + str(i) + ": "))
+    if price > 0:
+        prices.append(price)
+    else:
+        print("Invalid input. Enter a positive number.")
+        break
+
+|

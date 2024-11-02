835559c9ae648a7f74d22906edc66978200196a4
alielajami <alielajami@outlook.com>
2024-11-02 17:58:41 +0200

    linting the code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+""" Stock Market Profits"""
 
 days = int(input("Enter the number of days: "))
 prices = []
@@ -11,9 +12,10 @@ for i in range(1, days+1):
         break
 
 def max_profit(prices):
+    '''Function to calculate the maximum profit that can be made from the stock prices'''
     min_price = prices[0]
     min_day = 1
-    max_profit = 0
+    maxprofit = 0
     buy_day = 1
     sell_day = 1
 
@@ -21,17 +23,18 @@ def max_profit(prices):
         if prices[j] < min_price:
             min_price = prices[j]
             min_day = j + 1
-        elif prices[j] - min_price > max_profit:
-            max_profit = prices[j] - min_price
+        elif prices[j] - min_price > maxprofit:
+            maxprofit = prices[j] - min_price
             buy_day = min_day
             sell_day = j + 1
 
-    return max_profit, buy_day, sell_day
+    return maxprofit, buy_day, sell_day
 
-profit, buy_day, sell_day = max_profit(prices)
+profit, buyday, sellday = max_profit(prices)
 if profit > 0:
     print(f"Maximum profit: {profit} $")
-    print(f"Buy on day {buy_day} and sell on day {sell_day}")
+    print(f"Buy on day {buyday} and sell on day {sellday}")
 else:
     print("No profit can be made")
-# we can also add the print lines inside the function but the main goal was for the function to return
+# we can also add the print lines inside the function
+#  but the main goal was for the function to return
